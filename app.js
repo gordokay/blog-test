c7a6a5e671641985737212c08bba5c50b3d828f9
Kayleigh <57508868+gordokay@users.noreply.github.com>
2023-03-11 10:42:08 -0500

    Add token authentication

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ require("express-async-errors");
 
 const blogsRouter = require("./controllers/blogs");
 const usersRouter = require("./controllers/users");
+const loginRouter = require("./controllers/login");
 const middleware = require("./utils/middleware");
 const config = require("./utils/config");
 const logger = require("./utils/logger");
@@ -33,6 +34,7 @@ app.use(morgan("tiny"));
 
 app.use("/api/blogs", blogsRouter);
 app.use("/api/users", usersRouter);
+app.use("/api/login", loginRouter);
 
 app.use(middleware.unknownEndpoint);
 app.use(middleware.errorHandler);
---
+++ controllers/login.js
@@ -0,0 +1,24 @@
+const loginRouter = require("express").Router();
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
+const User = require("../models/user");
+const config = require("../utils/config");
+
+loginRouter.post("/", async (req, res) => {
+  const { username, password } = req.body;
+  const user = await User.findOne({ username });
+  const isPasswordCorrect =
+    user === null ? false : await bcrypt.compare(password, user.passwordHash);
+  if (!user || !isPasswordCorrect) {
+    return res.status(401).json({ error: "Invalid username or password" });
+  }
+  const tokenUser = {
+    username: user.username,
+    id: user._id,
+  };
+
+  const token = jwt.sign(tokenUser, config.SECRET);
+  res.status(200).send({ token, username: user.username, name: user.name });
+});
+
+module.exports = loginRouter;
