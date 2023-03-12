de17eda956cfdd3d6ac42244e03ec73937b30316
Kayleigh <57508868+gordokay@users.noreply.github.com>
2023-03-12 11:50:24 -0400

    Add token authorization middleware

--- utils/middleware.js
+++ utils/middleware.js
@@ -1,4 +1,25 @@
 const logger = require("./logger");
+const config = require("./config");
+const jwt = require("jsonwebtoken");
+const User = require("../models/user");
+
+const userExtractor = async (req, res, next) => {
+  const auth = req.get("authorization");
+  if (auth && auth.startsWith("Bearer ")) {
+    const decodedToken = jwt.verify(auth.replace("Bearer ", ""), config.SECRET);
+    if (!decodedToken.id) {
+      return res.status(401).json({ error: "Invalid token" });
+    }
+    const user = await User.findById(decodedToken.id);
+    if (!user) {
+      return res.status(401).json({ error: "Invalid user" });
+    }
+    req.user = user.id;
+  } else {
+    return res.status(401).json({ error: "Authorization required" });
+  }
+  next();
+};
 
 const unknownEndpoint = (req, res) => {
   res.status(404).json({ error: "Unknown endpoint" });
@@ -12,8 +33,12 @@ const errorHandler = (error, req, res, next) => {
     return res.status(400).json({
       error: error.message,
     });
+  } else if (error.name === "JsonWebTokenError") {
+    return res.status(401).json({ error: error.message });
+  } else if (error.name === "TokenExpiredError") {
+    return res.status(401).error({ error: "Token expired" });
   }
   next(error);
 };
 
-module.exports = { unknownEndpoint, errorHandler };
+module.exports = { userExtractor, unknownEndpoint, errorHandler };
